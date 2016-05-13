34a3fb4d69738bf7e0630818464c49cc253cdaa4
bqbs <codemonkeyjc@gmail.com>
2016-05-13 14:48:52 +0800

    change

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+from xml import etree
 from flask import Flask
 from flask import request
 import hashlib
+import time
 
 app = Flask(__name__)
 
@@ -21,14 +23,14 @@ def get_wechat():
     nonce = request.args['nonce']
     echostr = request.args['echostr']
     # 自己的token
-    token = "bqbs"  #这里改写你在微信公众平台里输入的token
-    #字典序排序
+    token = "bqbs"  # 这里改写你在微信公众平台里输入的token
+    # 字典序排序
     list = [token, timestamp, nonce]
     list.sort()
     sha1 = hashlib.sha1()
     map(sha1.update, list)
     hashcode = sha1.hexdigest()
-    #sha1加密算法
+    # sha1加密算法
 
     #如果是来自微信的请求，则回复echostr
     if hashcode == signature:
@@ -36,9 +38,20 @@ def get_wechat():
         print echostr
         return echostr
 
+
 @app.route('/wchat', methods=['POST'])
-def post_wechat():
-    return "hello"
+def post_wechat(self):
+    if not request.args:
+        return False
+    str_xml = request.data
+
+    xml = etree.fromstring(str_xml)  # 进行XML解析
+    content = xml.find("Content").text  # 获得用户所输入的内容
+    msgType = xml.find("MsgType").text
+    fromUser = xml.find("FromUserName").text
+    toUser = xml.find("ToUserName").text
+    return self.render.reply_text(fromUser, toUser, int(time.time()), u"我现在还在开发中，还没有什么功能，您刚才说的是：" + content)
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=80, debug=True)
